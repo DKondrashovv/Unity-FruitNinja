ed02e2f1610c3514a3fb0fb3071f3286319dda55
KonDan9 <danik.kondrashov.8@gmail.com>
2021-12-01 15:45:58 +0300

    Add fruitNinja

---
+++ Assets/Course Library/Scripts/Target.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    [SerializeField] private ParticleSystem Particle;
+    private GameManager gameManager;
+    [SerializeField] private int pointValue;
+    [SerializeField] private float minSpeed = 25;
+    [SerializeField] private float maxSpeed = 30;
+    [SerializeField] private float maxTorque = 12;
+    [SerializeField] private float xRange = 6;
+    [SerializeField] private float ySpawnPos = -6;
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+    private void OnMouseDown()
+    {   
+        Destroy(gameObject);
+        Instantiate(Particle, transform.position, Quaternion.identity);
+        gameManager.UpdateScore(pointValue);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Course Library/_Source_Files/Scripts/DestroyAfterDelay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterDelay : MonoBehaviour
+{
+   private bool isAlive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAlive==false)
+        {
+
+        }
+    }
+}
